bfa488a40a017da26add968b6ae0dd50e98cc613
Pratik Shitole <Pratik.Shitole@deltatre.com>
2024-06-18 11:11:43 +0530

    Objects and constuctor

---
+++ src/ClassesAndObjects/ObjectDemo2.java
@@ -0,0 +1,19 @@
+package ClassesAndObjects;
+
+import java.util.ArrayList;
+
+public class ObjectDemo2
+{
+    public static void main(String[] args)
+    {
+    ObjectsDemo obj = new ObjectsDemo("Ram");
+    ObjectsDemo obj1= new ObjectsDemo();
+    /*ObjectsDemo obj2= new ObjectsDemo();
+    obj.i=40;
+    obj1.i=50;
+    System.out.println(obj.i);
+    System.out.println(obj1.i);
+    System.out.println(obj2.i);*/
+        ArrayList<Integer> arrlist =new ArrayList<>(20);
+    }
+}
